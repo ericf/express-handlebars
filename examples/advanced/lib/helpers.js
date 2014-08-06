03c16a0a70bbf650f12c9841747ed79d3ce7ac47
ycao2 <ycao@walmartlabs.com>
2014-08-06 16:21:18 -0700

    add hbs helper to enable extending css js in advanced example

--- examples/advanced/lib/helpers.js
+++ examples/advanced/lib/helpers.js
@@ -1,3 +1,16 @@
 exports.yell = function (msg) {
     return msg.toUpperCase();
 };
+
+var blocks = {};
+
+exports.block = function (name) {
+  var str = (blocks[name] || []).join("");
+  blocks[name] = [];
+  return str;
+};
+
+exports.extend = function (name, options) {
+  if (!blocks[name]) return;
+  blocks[name].push(options.fn(this));
+};
