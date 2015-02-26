9d218a994048f2d6a4d433e58c7bda8c8629782e
Jeff Conniff <jeffconniff@yahoo.com>
2015-02-26 10:36:02 -0800

    Adding/updating copyright info

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2014, Yahoo Inc. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
 'use strict';
 
 var ExpressHandlebars = require('./lib/express-handlebars');
